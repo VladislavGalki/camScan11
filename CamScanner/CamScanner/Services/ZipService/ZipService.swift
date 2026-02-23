0b4f35dea51fe1c0c0098c1664ef0d949001574e
Владислав Галкин <vladislavgalkin@MacBook-Pro-Vladislav.local>
2026-02-23 23:44:59 +0300

    fix zip

--- CamScanner/CamScanner/Services/ZipService/ZipService.swift
+++ CamScanner/CamScanner/Services/ZipService/ZipService.swift
@@ -4,21 +4,44 @@ import ZIPFoundation
 final class ZipService {
     static let shared = ZipService()
     private init() {}
-    
+
     func zip(files: [URL], fileName: String) throws -> URL {
-        let url = FileManager.default.temporaryDirectory
+        let fm = FileManager.default
+
+        let zipURL = fm.temporaryDirectory
             .appendingPathComponent(fileName)
             .appendingPathExtension("zip")
 
-        let archive = try Archive(url: url, accessMode: .create)
-
-        for file in files {
-            try archive.addEntry(
-                with: file.lastPathComponent,
-                fileURL: file
-            )
+        if fm.fileExists(atPath: zipURL.path) {
+            do {
+                try fm.removeItem(at: zipURL)
+            } catch {
+                throw error
+            }
         }
 
-        return url
+        let archive = try Archive(url: zipURL, accessMode: .create)
+
+        for file in files {
+            guard fm.fileExists(atPath: file.path) else {
+                continue
+            }
+
+            do {
+                try archive.addEntry(
+                    with: file.lastPathComponent,
+                    fileURL: file,
+                    compressionMethod: .deflate
+                )
+            } catch {
+                throw error
+            }
+        }
+        
+        return zipURL
     }
+}
+
+enum ZipError: Error {
+    case createFailed
 }
